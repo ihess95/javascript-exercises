9801d00f8d06f42c1a96ba84788f62532121676d
Ian Hess <ian.h.hess@gmail.com>
2022-10-11 14:12:25 -0400

    finish test 4

--- javascript-exercises/04_removeFromArray/removeFromArray.js
+++ javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, toRemove) {
+  const args = Array.from(arguments);
+  for (let i = 0; i < arguments.length; i++) {
+    const index = arr.indexOf(arguments[i]);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
