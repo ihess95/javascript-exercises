043cfe3308d44abae3ad48734c4db200b65535ce
Ian Hess <ian.h.hess@gmail.com>
2022-10-11 11:47:26 -0400

    complete third exercise

---
+++ javascript-exercises/03_reverseString/reverseString.js
@@ -0,0 +1,9 @@
+const reverseString = function (input) {
+  const splitString = input.split("");
+  const reverseArray = splitString.reverse();
+  const joinArray = reverseArray.join("");
+  return joinArray;
+};
+
+// Do not edit below this line
+module.exports = reverseString;
