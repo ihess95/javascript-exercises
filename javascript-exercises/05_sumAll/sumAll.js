c7cafbd56983e4faf6e5f4feec459dc18a9cdbd1
Ian Hess <ian.h.hess@gmail.com>
2022-10-11 14:34:31 -0400

    complete exercise 5

--- javascript-exercises/05_sumAll/sumAll.js
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let total = 0;
+  let smallerNumber = num1;
+  let largerNumber = num2;
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    typeof num1 !== "number" ||
+    typeof num2 !== "number"
+  ) {
+    return "ERROR";
+  } else {
+    if (num2 < num1) {
+      smallerNumber = num2;
+      largerNumber = num1;
+    }
+    for (let i = smallerNumber; i <= largerNumber; i++) {
+      total += i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
