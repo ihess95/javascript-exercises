52ca5157ada06e940da130ecd2e1583a80e8e258
Ian Hess <ian.h.hess@gmail.com>
2022-10-12 15:39:44 -0400

    solve exercise 7

--- javascript-exercises/07_tempConversion/tempConversion.js
+++ javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  const result = (temp - 32) * (5 / 9);
+  return parseFloat(result.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  const result = temp * (9 / 5) + 32;
+  return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
