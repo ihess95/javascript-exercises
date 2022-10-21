e4095241c42ae2d4c98fad452e0351150c72e2b5
Ian Hess <ian.h.hess@gmail.com>
2022-10-21 10:28:50 -0400

    solve fibonacci exercise

--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  let x = 0;
+  let y = 1;
+  if (n === 0) return 0;
+  if (n < 0) return "OOPS";
+  for (let i = 1; i < n; i++) {
+    const temp = y;
+    y = x + y;
+    x = temp;
+  }
 
+  return y;
 };
 
 // Do not edit below this line
