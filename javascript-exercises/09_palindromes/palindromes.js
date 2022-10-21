8e81bf3fca5d1415186b6fabdc7d43a6925e7fe2
Ian Hess <ian.h.hess@gmail.com>
2022-10-21 09:53:00 -0400

    solve all tests

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -7,8 +7,16 @@ const palindromes = function (word) {
   const joined = reverse.join("");
   if (joined === punc) {
     return true;
+  } else {
+    return false;
   }
 };
+
+//Method used in Odin project
+// const palindromesP = function (string) {
+//   const pString = string.toLowerCase().replace(/[^a-z]/g, "");
+//   return pString.split("").reverse().join("") === pString;
+// };
 
 // Do not edit below this line
 module.exports = palindromes;
