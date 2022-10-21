ff04d29e3c4a56df4045707cd8ebf4e0530f6b93
Ian Hess <ian.h.hess@gmail.com>
2022-10-21 09:45:18 -0400

    solve test 4

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
 const palindromes = function (word) {
-  const caps = word.toLowerCase();
+  const noSpace = word.split(" ").join;
+  const caps = noSpace.toLowerCase();
   const punc = caps.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
   const arr = punc.split("");
   const reverse = arr.reverse();
