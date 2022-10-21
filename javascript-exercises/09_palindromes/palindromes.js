c2233873d7fcaa5402e03c919c7843a160fb8936
Ian Hess <ian.h.hess@gmail.com>
2022-10-21 09:41:42 -0400

    create testing file and complete 3 tests

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const caps = word.toLowerCase();
+  const punc = caps.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+  const arr = punc.split("");
+  const reverse = arr.reverse();
+  const joined = reverse.join("");
+  if (joined === punc) {
+    return true;
+  }
 };
 
 // Do not edit below this line
